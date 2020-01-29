c970e1bb179180128751bedfaf3b25703eb1e772
alopsantos <anderson_lops@hotmail.com>
2020-01-28 23:21:39 -0300

    chore: Criando loader de models

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+import Sequelize from 'sequelize';
+import User from '../app/models/User';
+import Databaseconfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+  init() {
+    this.connection = new Sequelize(Databaseconfig)
+
+    models.map(model => model.init(this.connection));
+  }
+}
+export default new Database();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'World' })
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: "Anderson",
+    email: "anderson@lopscorp.com",
+    password_hash: "32323323",
+  })
+  return res.json(user);
 })
 
 export default routes;
