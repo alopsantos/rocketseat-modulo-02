44fde50bd9da653af164185bcde178c3b94c3077
alopsantos <anderson_lops@hotmail.com>
2020-02-07 00:16:32 -0300

    chore: upload do avatar concluido

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserControler from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -6,6 +9,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserControler.store);
 routes.post('/sessions', SessionController.store);
@@ -13,5 +17,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddlewares);
 
 routes.put('/users', UserControler.update);
-
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+})
 export default routes;
